629a4c4daa4209270bfb2c7f7349f2a9c7d5a6f6
ChristophHu <christoph.huschenhoefer@gmx.de>
2023-06-15 12:01:16 +0200

    add lab-code.local

--- projects/adfs-demo/src/app/auth.config.ts
+++ projects/adfs-demo/src/app/auth.config.ts
@@ -5,6 +5,7 @@ export const authModuleConfig: OAuthModuleConfig = {
   resourceServer: {
     allowedUrls: [
       'https://dc2019.poldom.local/adfstestapi',
+      'https://dc2022.lab-code.local/adfs',
       'http://localhost:4200'
     ],
     sendAccessToken: true
@@ -12,8 +13,14 @@ export const authModuleConfig: OAuthModuleConfig = {
 }
 
 export const authConfig: AuthConfig = {
-  issuer        : 'https://dc2019.poldom.local/adfs',
-  clientId      : '0050e983-4a4e-4076-8d0d-a1b625c4aa06',
+  // lab-code.local
+  issuer        : 'https://dc2022-v-01.lab-code.local/adfs',
+  clientId      : '0ab63671-ab50-4e60-be3a-40a30dbf4c14',
+  
+  // poldom.local
+  // issuer        : 'https://dc2019.poldom.local/adfs',
+  // clientId      : '0050e983-4a4e-4076-8d0d-a1b625c4aa06',
+  
   redirectUri   : 'http://localhost:4200/',
   scope         : 'openid profile email api',
   useSilentRefresh: false,
